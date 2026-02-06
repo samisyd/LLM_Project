4b4e11a584b10dc4173f3df0e8176a9a6dab9aef
samisyd <samina.syd@gmail.com>
2026-02-05 20:47:08 -0800

    fixed bug to return doctors voice as output file

--- src/doctors_voice.py
+++ src/doctors_voice.py
@@ -10,6 +10,7 @@ def text_to_speech_with_gtts(input_text, output_filepath):
         slow=False
     )    
     audioobj.save(output_filepath)
+    return output_filepath
 
 # input_text="Hi this is Ai with Samina!"
 # text_to_speech_with_gtts(input_text, output_filepath="gtts_autoplay.mp3")
